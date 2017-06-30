32037afadf765ff333b5a48d584b08569402243a
Nikolaos Kavvadias <kavvadias@silexica.com>
2017-06-30 11:12:56 +0200

    Clean up verilogieee

--- verilogieee/elemapprox.v
+++ verilogieee/elemapprox.v
@@ -1,5 +1,3 @@
-`include "constants.v"
-
 module elemapprox;
 
 function real fabs;
--- verilogieee/plot-verilog-pbm.sh
+++ verilogieee/plot-verilog-pbm.sh
@@ -2,8 +2,7 @@
 
 #export PATH=/c/CompSci/iverilog/bin:$PATH
 
-iverilog -g2005 -E -o elemapproxpp.v constants.v elemapprox.v
-iverilog -g2005 -o testfunc.vvp elemapproxpp.v graph.v funcplot.v testfunc.v
+iverilog -g2005 -o testfunc.vvp elemapprox.v graph.v funcplot.v testfunc.v
 for app in "sin" "cos" "tan" "cot" "sec" "csc" \
   "asin" "acos" "atan" "acot" "asec" "acsc" \
   "sinh" "cosh" "tanh" "coth" "sech" "csch" \
