7841e50d796f2cbae1512d9e38d6f66f4c329097
Nikolaos Kavvadias <kavvadias@silexica.com>
2017-07-01 10:18:26 +0200

    PBM-to-PNG using convert

---
+++ common/scripts/pbm2png.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ../../ansicstd
+
+for app in "sin" "cos" "tan" "cot" "sec" "csc" \
+  "asin" "acos" "atan" "acot" "asec" "acsc" \
+  "sinh" "cosh" "tanh" "coth" "sech" "csch" \
+  "asinh" "acosh" "atanh" "acoth" "asech" "acsch" \
+  "exp" "log" "sqrt" \
+  "floor" "ceil" "round" "trunc" "fabs"
+do
+  convert test${app}.pbm ../figures/test${app}.png
+done
+
+cd ../common/scripts
+
